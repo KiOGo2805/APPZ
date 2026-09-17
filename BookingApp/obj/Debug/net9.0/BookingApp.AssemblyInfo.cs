f05c11ef82382a38485ae03491c5ac6f7ce92ca0
Stepan Melnyk <stepan.melnyk2007@gmail.com>
2026-09-17 15:32:37 +0300

    Add initial build artifacts and project configuration for BookingApp

--- BookingApp/obj/Debug/net9.0/BookingApp.AssemblyInfo.cs
+++ BookingApp/obj/Debug/net9.0/BookingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+605c022792eea6f2ba86cde4066eed551d62aa30")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
