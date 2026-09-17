5f627c627b7a9afb23ac6aeaf761931c0ec521b5
Kawoonchik <sydor.anatolii@gmail.com>
2026-09-17 16:03:09 +0300

    Penis2

--- BookingApp/Controllers/ReservationController.cs
+++ BookingApp/Controllers/ReservationController.cs
@@ -30,7 +30,6 @@ public class ReservationController
 
         if (availableTable != null)
         {
-            // Патерн Creator: Контролер створює Reservation, оскільки ініціює цей процес і має потрібні дані
             Reservation newReservation = new Reservation
             {
                 Id = _reservationIdCounter++,
